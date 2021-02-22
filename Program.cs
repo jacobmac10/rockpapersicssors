c8d69bfdbc878886d4e526513a98147e6909b647
Benjamin Macias <jacobmac10@hotmail.com>
2021-02-22 10:13:33 -0700

    se agrega seleccion aleatoria de juego

--- Program.cs
+++ Program.cs
@@ -21,6 +21,20 @@ namespace RockPaperScissors
            int option = Convert.ToInt32(Console.ReadLine());
            return option;
         }
+
+        /// <summary>
+        /// se selecciona opcion aleatoria entre 1,2 o 3
+        /// </summary>
+        /// <returns>la opcion seleccionada, 1-piedra, 2-papel, 3-tijera</returns>
+
+        static int SelectRandomOption()
+        {
+          
+            Random rnd = new Random();
+            int option  = rnd.Next(1, 4);  // 1,2 0 3
+            return option;
+            
+        }
         static void Main(string[] args)
         {
 
@@ -29,8 +43,11 @@ namespace RockPaperScissors
             // 2: papel
             // 3: tijera
             int option = selectOption();
-            Console.WriteLine("Opcion seleccionada"+ option);
+            Console.WriteLine("Opcion seleccionada:" + option);
             //TODO: La computadora genera su opción a jugar de manera aleatoria (piedra, papel o tijera)
+            int gameOption = SelectRandomOption();
+            Console.WriteLine("valor seleccionado aleatoriamente:" + gameOption);
+
 
             //TODO: Determinar quién ganó de acuerdo a las opciones seleccionadas, y mostrar al ganador
 
